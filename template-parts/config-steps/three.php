1f8c7161d63ade664d7cda7780afe64c6488cd6e
Tlegen Bayangali <tlegmusic@gmail.com>
2022-02-10 14:21:26 +0500

    config

--- template-parts/config-steps/three.php
+++ template-parts/config-steps/three.php
@@ -12,10 +12,6 @@
     foreach ($configs->posts as $config) {
         $useful_configs[] = $config;
     }
-
-    echo '<pre>';
-    print_r($useful_configs);
-    echo '</pre>';
 ?>
 
 <div class="content complections pt-60 pb-60 pl-80">
