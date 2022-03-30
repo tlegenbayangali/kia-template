56f53cfe066d572c25314d0660e085b4234ea06b
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2022-03-30 13:13:36 +0500

    cleanPhone function regexed

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -8,45 +8,53 @@
 /**
  * Adds custom classes to the array of body classes.
  *
- * @param array $classes Classes for the body element.
+ * @param  array  $classes  Classes for the body element.
  * @return array
  */
-function kia_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
+function kia_body_classes($classes)
+{
+    // Adds a class of hfeed to non-singular pages.
+    if (!is_singular()) {
+        $classes[] = 'hfeed';
+    }
 
-	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
-	}
+    // Adds a class of no-sidebar when there is no sidebar present.
+    if (!is_active_sidebar('sidebar-1')) {
+        $classes[] = 'no-sidebar';
+    }
 
-	return $classes;
+    return $classes;
 }
-add_filter( 'body_class', 'kia_body_classes' );
+
+add_filter('body_class', 'kia_body_classes');
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function kia_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
-	}
+function kia_pingback_header()
+{
+    if (is_singular() && pings_open()) {
+        printf('<link rel="pingback" href="%s">', esc_url(get_bloginfo('pingback_url')));
+    }
 }
-add_action( 'wp_head', 'kia_pingback_header' );
+
+add_action('wp_head', 'kia_pingback_header');
 
 /**
  * Функция для очистки телефона
  */
-function cleanPhone($string) {
-	return str_replace([' ', '(', ')', '-'], "", $string);
+function cleanPhone($string)
+{
+    return preg_replace('/[^0-9\+]/', '', $string);
+//	return str_replace([' ', '(', ')', '-'], "", $string);
 }
 
-function toInt($str) {
-	return (int)preg_replace('/\s/', '', $str);
+function toInt($str)
+{
+    return (int) preg_replace('/\s/', '', $str);
 }
 
-function price_for_month($field_name, $id, $months) {
-	return number_format(toInt(get_field($field_name, $id)) / $months, 0, ' ', ' ');
+function price_for_month($field_name, $id, $months)
+{
+    return number_format(toInt(get_field($field_name, $id)) / $months, 0, ' ', ' ');
 }
--- template-contacts.php
+++ template-contacts.php
@@ -4,97 +4,111 @@ Template Name: Контакты
 */
 get_header()
 ?>
-<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="fz-35">
-                Контакты
-            </h1>
+<?php
+get_template_part('template-parts/breadcrumbs'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="fz-35">
+                    Контакты
+                </h1>
+            </div>
         </div>
     </div>
-</div>
-<hr>
-<div class="pb-60">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12 col-md-6" itemscope itemtype="https://schema.org/Organization">
-                <h2 class="fz-50" itemprop>
-                    <?php the_field('site_name', 'options'); ?>
-                </h2>
-                <p class="mt-20" itemprop="description">
-                    <?php the_field('site_description', 'options'); ?>
-                </p>
+    <hr>
+    <div class="pb-60">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6" itemscope itemtype="https://schema.org/Organization">
+                    <h2 class="fz-50" itemprop>
+                        <?php
+                        the_field('site_name', 'options'); ?>
+                    </h2>
+                    <p class="mt-20" itemprop="description">
+                        <?php
+                        the_field('site_description', 'options'); ?>
+                    </p>
                     <div class="mt-20 btn-wrapper btn-wrapper-lg btn-wrapper-black">
                         <a href="/callback/" class="btn">Заказать звонок</a>
                     </div>
-                <hr>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="contacts-info-group">
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="contacts-info-group">
                             <span class="small">
                                 Адрес
                             </span>
-                            <span class="info" itemprop="address">
-                                <?php the_field('address', 'options') ?>
+                                <span class="info" itemprop="address">
+                                <?php
+                                the_field('address', 'options') ?>
                             </span>
-                        </div>
-                        <div class="contacts-info-group">
+                            </div>
+                            <div class="contacts-info-group">
                             <span class="small">
                                 Юридическое лицо
                             </span>
-                            <span class="info" itemprop="legalName">
-                                <?= get_field('dealer_info', 'options')['dealer_name'] ?>
+                                <span class="info" itemprop="legalName">
+                                <?= get_field('dealer_info', 'options')[ 'dealer_name' ] ?>
                             </span>
-                        </div>
-                        <div class="contacts-info-group">
+                            </div>
+                            <div class="contacts-info-group">
                             <span class="small">
                                 Режим работы
                             </span>
-                            <span class="info">
-                                <?= get_field('dealer_info', 'options')['dealer_schedule']; ?>
+                                <span class="info">
+                                <?= get_field('dealer_info', 'options')[ 'dealer_schedule' ]; ?>
                             </span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 contacts-phone">
-                        <?php if (get_field('dealer_info', 'options')['dealer_phones']) : ?>
-                        <div class="contacts-info-group">
+                        <div class="col-md-6 contacts-phone">
+                            <?php
+                            if (get_field('dealer_info', 'options')[ 'dealer_phones' ]) : ?>
+                                <div class="contacts-info-group">
                             <span class="small">
                                 Телефоны
                             </span>
-                            <span class="info d-flex flex-column">
-                                <?php foreach (get_field('dealer_info', 'options')['dealer_phones'] as $phone) : ?>
-                                <a href="tel:<?= cleanPhone($phone['dealer_phone']) ?>" class="right-number pos-r d-block">
-                                    <span itemprop="telephone"><?= $phone['dealer_phone'] ?></span>
+                                    <span class="info d-flex flex-column">
+                                <?php
+                                foreach (get_field('dealer_info', 'options')[ 'dealer_phones' ] as $phone) : ?>
+                                    <a href="tel:<?= cleanPhone($phone[ 'dealer_phone' ]) ?>" class="right-number pos-r d-block">
+                                    <span itemprop="telephone"><?= $phone[ 'dealer_phone' ] ?></span>
                                 </a>
-                                <?php endforeach; ?>
+                                <?php
+                                endforeach; ?>
                             </span>
-                        </div>
-                        <?php endif; ?>
-                        <?php if (get_field('dealer_info', 'options')['email']) : ?>
-                        <div class="contacts-info-group">
+                                </div>
+                            <?php
+                            endif; ?>
+                            <?php
+                            if (get_field('dealer_info', 'options')[ 'email' ]) : ?>
+                                <div class="contacts-info-group">
                             <span class="small">
                                 Email
                             </span>
-                            <span class="info d-flex flex-column">
-                                <?php foreach (get_field('dealer_info', 'options')['email'] as $email) : ?>
-                                <a href="mailto:<?= $email['email_item'] ?>" class="right-number pos-r d-block">
-                                    <span itemprop="email"><?= $email['email_item'] ?></span>
+                                    <span class="info d-flex flex-column">
+                                <?php
+                                foreach (get_field('dealer_info', 'options')[ 'email' ] as $email) : ?>
+                                    <a href="mailto:<?= $email[ 'email_item' ] ?>" class="right-number pos-r d-block">
+                                    <span itemprop="email"><?= $email[ 'email_item' ] ?></span>
                                 </a>
-                                <?php endforeach; ?>
+                                <?php
+                                endforeach; ?>
                             </span>
+                                </div>
+                            <?php
+                            endif; ?>
                         </div>
-                        <?php endif; ?>
                     </div>
                 </div>
+                <?php
+                if (get_field('dealer_info', 'options')[ 'dealer_map' ]) : ?>
+                    <div class="col-12 col-md-6 asd">
+                        <?= get_field('dealer_info', 'options')[ 'dealer_map' ] ?>
+                    </div>
+                <?php
+                endif; ?>
             </div>
-            <?php if (get_field('dealer_info', 'options')['dealer_map']) : ?>
-            <div class="col-12 col-md-6 asd">
-                <?= get_field('dealer_info', 'options')['dealer_map'] ?>
-            </div>
-            <?php endif; ?>
         </div>
     </div>
-</div>
-<?php 
+<?php
 get_footer();
