a9070ebb6989b0946fcf6960f52e5d9de8d3dbf7
Sagyndyk <sagindik2003@gmail.com>
2022-05-16 16:37:05 +0500

    add show cart heading

--- template-parts/content-offers-service.php
+++ template-parts/content-offers-service.php
@@ -3,31 +3,33 @@
         <div class="offers-card">
             <div class="img">
                 <a href="<?= get_the_permalink() ?>">
-                    <?= get_the_post_thumbnail( get_the_ID(), 'full' ) ?>
+                    <?= get_the_post_thumbnail(get_the_ID(), 'full') ?>
                 </a>
             </div>
             <div class="title">
                 <div class="d-flex flex-column">
-                    <a href="<?= get_the_permalink() ?>">
-                        <span class="mr-2 underlined-black fz-15 d-none fw-700">
-                            <?= get_the_title() ?>
-                        </span>
-                    </a>
+                    <?php if (get_field('show_cart_heading', 'options')) : ?>
+                        <a href="<?= get_the_permalink() ?>">
+                            <span class="mr-2 underlined-black fz-15 fw-700">
+                                <?= get_the_title() ?>
+                            </span>
+                        </a>
+                    <?php endif; ?>
                     <p class="c-disabled mt-10">
                         <?php
-                            $now = new DateTime('Asia/Oral');
+                        $now = new DateTime('Asia/Oral');
 
-                            $period = get_field('period', get_the_ID());
-                            $date_start = DateTime::createFromFormat('Y-m-d', $period[ 'period_start' ]);
-                            $date_end = DateTime::createFromFormat('Y-m-d', $period[ 'period_end' ]);
-                            if ($date_start && $date_end) {
-                                $duration = $date_end->diff($date_start);
-                                $left = $date_end->diff($now);
-                            }
+                        $period = get_field('period', get_the_ID());
+                        $date_start = DateTime::createFromFormat('Y-m-d', $period['period_start']);
+                        $date_end = DateTime::createFromFormat('Y-m-d', $period['period_end']);
+                        if ($date_start && $date_end) {
+                            $duration = $date_end->diff($date_start);
+                            $left = $date_end->diff($now);
+                        }
                         ?>
                         <?php if ($date_start && $date_end) : ?>
                             <?php if ($now >= $date_start && $now <= $date_end) : ?>
-                                Осталось: 
+                                Осталось:
                                 <?php if ($left->d == 0 || $left->d >= 5) : ?>
                                     <?= $left->d ?> дней
                                 <?php elseif ($left->d == 1) : ?>
