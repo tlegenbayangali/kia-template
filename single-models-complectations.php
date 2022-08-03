1af95e42c2a4d969212a55307c09f4bd00613175
Sagyndyk <sagindik2003@gmail.com>
2022-08-03 18:03:29 +0500

    030822 add multimedia for config new options

--- single-models-complectations.php
+++ single-models-complectations.php
@@ -10394,6 +10394,34 @@ $GLOBALS['model_min_price'] = $model_min_price;
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="equip-config-section-item">
+                                            <div class="equip-config-section-item-header">
+                                                Мультимедиа 8" с Apple Carplay и Android Auto *
+                                            </div>
+                                            <div class="equip-config-section-item-carousel">
+                                                <!-- SWIPER STARTS-->
+                                                <div class="swiper-container equip-config-section-item-carousel-container">
+                                                    <div class="swiper-wrapper equip-config-section-item-carousel-wrapper">
+
+                                                        <?php foreach ($configs->posts as $post) : ?>
+                                                            <?php $multimedia = get_field('multimedia_options', $post->ID); ?>
+                                                            <!-- SWIPER ITEMS START-->
+                                                            <div class="swiper-slide equip-config-section-item-carousel-slide">
+                                                                <div>
+                                                                    <?php if ($multimedia['multimedia_8_s_apple_carplay_i_android_auto']) : ?>
+                                                                        <svg>
+                                                                            <use xlink:href="<?php echo get_template_directory_uri() ?>/dist/images/dist/sprite.svg#check"></use>
+                                                                        </svg>
+                                                                    <?php else : ?>
+                                                                        <span class="d-block"> — </span>
+                                                                    <?php endif; ?>
+                                                                </div>
+                                                            </div>
+                                                        <?php endforeach; ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                         <div class="equip-config-section-items">
                                             <!-- ONE SECTION CAROUSEL ITEM-->
                                             <div class="equip-config-section-item">
