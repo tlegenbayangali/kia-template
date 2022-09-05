9c9b8219a04ee58dfb55cb1a7374156b7ac3567c
Sagyndyk <sagindik2003@gmail.com>
2022-09-05 16:35:54 +0500

    asd

--- single-online-services-test-drive.php
+++ single-online-services-test-drive.php
@@ -2,15 +2,15 @@
 get_header();
 ?>
 <?php get_template_part('template-parts/breadcrumbs'); ?>
-<?php if (get_field('forma_test-drajva', 'options')) : ?>
-    <?php if (!isset($_POST['selected_model'])) : ?>
-        <?php get_template_part('template-parts/test-drive', 'selector'); ?>
-    <?php else : ?>
-        <?php get_template_part('template-parts/test-drive', 'form'); ?>
-    <?php endif; ?>
-<?php else : ?>
-    <?php get_template_part('template-parts/test-drive-new-form', 'form'); ?>
-<?php endif; ?>
+<!-- </?php if (get_field('forma_test-drajva', 'options')) : ?> -->
+<!-- </?php //if (!isset($_POST['selected_model'])) : ?> -->
+<!-- </?php get_template_part('template-parts/test-drive', 'selector'); ?> -->
+<!-- </?php else : ?> -->
+<!-- </?php get_template_part('template-parts/test-drive', 'form'); ?> -->
+<!-- </?php endif; ?> -->
+<!-- </?php else : ?> -->
+<?php get_template_part('template-parts/test-drive-new-form', 'form'); ?>
+<!-- </?phpendif; ?> -->
 
 
 <?php get_footer(); ?>
