8eadf8533983219ddc3cbd62f73803783a15697b
Sagyndyk <sagindik2003@gmail.com>
2022-09-05 17:42:45 +0500

    true false 1

--- single-online-services-test-drive.php
+++ single-online-services-test-drive.php
@@ -2,30 +2,16 @@
 get_header();
 ?>
 <?php get_template_part('template-parts/breadcrumbs'); ?>
-<!-- </?php if (get_field('forma_test-drajva', 'options')) : ?> -->
-<!-- </?php //if (!isset($_POST['selected_model'])) : ?> -->
-<!-- </?php get_template_part('template-parts/test-drive', 'selector'); ?> -->
-<!-- </?php else : ?> -->
-<!-- </?php get_template_part('template-parts/test-drive', 'form'); ?> -->
-<!-- </?php endif; ?> -->
-<!-- </?php else : ?> -->
-<div class="section section-divided pos-r">
-    <?php if (get_field('form-maps', 'options')) : ?>
-        <?= get_field('form-maps', 'options') ?>
+
+<?php if (get_field('forma_test-drajva', 'options')) : ?>
+    <?php if (!isset($_POST['selected_model'])) : ?>
+        <?php get_template_part('template-parts/test-drive', 'selector'); ?>
+    <?php else : ?>
+        <?php get_template_part('template-parts/test-drive', 'form'); ?>
     <?php endif; ?>
-    <div class="container">
-        <div class="col-xl-8 col-lg-12">
-            <div class="content callback-col pt-60 pb-60">
-                <div class="callback-form" style="margin: 0;">
-                    <h5 class="mb-2">Ваши контакты</h5>
-                    <p>Поля, отмеченные *, обязательны для заполнения</p>
-                    <?= do_shortcode('[contact-form-7 id="96" title="Тест-драйв"]') ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- </?phpendif; ?> -->
+<?php else : ?>
+    <?php get_template_part('template-parts/test-drive-new-form', 'form'); ?>
+<?php endif; ?>
 
 
 <?php get_footer(); ?>
