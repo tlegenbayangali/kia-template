11b02d18a9a40fdb4b651243ae5c59feb394ddaa
Sagyndyk <sagindik2003@gmail.com>
2022-09-05 17:49:53 +0500

    true false 6

--- single-online-services-test-drive.php
+++ single-online-services-test-drive.php
@@ -10,8 +10,22 @@ get_header();
         <?php get_template_part('template-parts/test-drive', 'form'); ?>
     <?php endif; ?>
 <?php else : ?>
-    <h1>safasfasfasgasgasg</h1>
-    <?php get_template_part('template-parts/test-drive-new-form', 'form'); ?>
+    <div class="section section-divided pos-r">
+        <?php if (get_field('form-maps', 'options')) : ?>
+            <?= get_field('form-maps', 'options') ?>
+        <?php endif; ?>
+        <div class="container">
+            <div class="col-xl-8 col-lg-12">
+                <div class="content callback-col pt-60 pb-60">
+                    <div class="callback-form" style="margin: 0;">
+                        <h5 class="mb-2">Ваши контакты</h5>
+                        <p>Поля, отмеченные *, обязательны для заполнения</p>
+                        <?= do_shortcode('[contact-form-7 id="96" title="Тест-драйв"]') ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php endif; ?>
 
 
