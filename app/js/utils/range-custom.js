1e174982db2af0fbe94ebce691f2eda702054e2b
Sagyndyk <sagindik2003@gmail.com>
2022-09-08 17:52:46 +0500

    fixed

--- app/js/utils/range-custom.js
+++ app/js/utils/range-custom.js
@@ -3,10 +3,11 @@ let rangeLabel = document.querySelector('.uacf7-slider-label')
 let rangeDate = document.querySelector('.form-range-value.uacf7-value')
 let rangeInputValue = document.querySelector('#form-range-value')
 
-const minRangeValue = range.getAttribute('Min')
-const maxRangeValue = range.getAttribute('Max')
+
 
 if (rangeLabel) {
+    const minRangeValue = range.getAttribute('Min')
+    const maxRangeValue = range.getAttribute('Max')
     rangeDate.innerHTML = '15:00 - 16:00'
     rangeLabel.innerHTML = `Время тест-драйва с ${parseInt(minRangeValue) - 1}:00 до ${maxRangeValue}:00`
     range.addEventListener('input', (event) => {
