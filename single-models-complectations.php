7bc45dc2d39e471ba536e04bb3bf83c9b5672cc7
Sagyndyk <sagindik2003@gmail.com>
2022-09-30 16:35:47 +0500

    complectations

--- single-models-complectations.php
+++ single-models-complectations.php
@@ -3913,34 +3913,7 @@ $GLOBALS['model_min_price'] = $model_min_price;
                                                             <div class="swiper-slide equip-config-section-item-carousel-slide">
                                                                 <div>
                                                                     <?php if ($exterior['svetodiodnaya_podsvetka_makiyazhnogo_zerkala_voditelya']) : ?>
-                                                                        <svg>
-                                                                            <use xlink:href="<?php echo get_template_directory_uri() ?>/dist/images/dist/sprite.svg#check"></use>
-                                                                        </svg>
-                                                                    <?php else : ?>
-                                                                        <span class="d-block"> — </span>
-                                                                    <?php endif; ?>
-                                                                </div>
-                                                            </div>
-                                                        <?php endforeach; ?>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="equip-config-section-item">
-                                            <div class="equip-config-section-item-header">
-                                                Светодиодная подсветка макияжного зеркала водителя
-                                            </div>
-                                            <div class="equip-config-section-item-carousel">
-                                                <!-- SWIPER STARTS-->
-                                                <div class="swiper-container equip-config-section-item-carousel-container">
-                                                    <div class="swiper-wrapper equip-config-section-item-carousel-wrapper">
-
-                                                        <?php foreach ($configs->posts as $post) : ?>
-                                                            <?php $exterior = get_field('interior_options', $post->ID); ?>
-                                                            <!-- SWIPER ITEMS START-->
-                                                            <div class="swiper-slide equip-config-section-item-carousel-slide">
-                                                                <div>
-                                                                    <?php if ($exterior['svetodiodnaya_podsvetka_makiyazhnogo_zerkala_voditelya']) : ?>
+                                                                        <?php var_dump($exterior) ?>
                                                                         <svg>
                                                                             <use xlink:href="<?php echo get_template_directory_uri() ?>/dist/images/dist/sprite.svg#check"></use>
                                                                         </svg>
