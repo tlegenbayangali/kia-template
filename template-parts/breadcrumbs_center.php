e3bf631f2cc95c66f4dcab69a321cbf88f9d3ade
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2022-10-31 13:42:25 +0500

    added breadcrumbs_center.php

---
+++ template-parts/breadcrumbs_center.php
@@ -0,0 +1,9 @@
+<div class="container breadcrumbs-container text-center asd">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="breadcrumbs">
+                <?php if( function_exists('kama_breadcrumbs') ) kama_breadcrumbs(); ?>
+            </div>
+        </div>
+    </div>
+</div>
