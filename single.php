a0b06b45dc1423451889bb0161521a9e0caef2f5
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2022-10-31 17:43:07 +0500

    input width fit full size

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="page">
-	<?php get_template_part( 'template-parts/breadcrumbs_center' ); ?>
+	<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-lg-7">
