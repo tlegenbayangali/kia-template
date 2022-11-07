8b57bdcd041b7729c2b3802a12b0b1a81a6d10ed
Sagyndyk <sagindik2003@gmail.com>
2022-11-07 16:36:16 +0500

    sistema_preduprezhdeniya_bokovogo_stolknoveniya_pri_vyezde_s_parkovki_zadnim_hodom_rccw

--- single-models-complectations.php
+++ single-models-complectations.php
@@ -5205,34 +5205,7 @@ $GLOBALS['model_min_price'] = $model_min_price;
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="equip-config-section-item">
-                                            <div class="equip-config-section-item-header">
-                                                Система предупреждения бокового столкновения при выезде с парковки задним ходом (RCCW)
-                                            </div>
-                                            <div class="equip-config-section-item-carousel">
-                                                <!-- SWIPER STARTS-->
-                                                <div class="swiper-container equip-config-section-item-carousel-container">
-                                                    <div class="swiper-wrapper equip-config-section-item-carousel-wrapper">
 
-                                                        <?php foreach ($configs->posts as $post) : ?>
-                                                            <?php $security = get_field('security_options', $post->ID); ?>
-                                                            <!-- SWIPER ITEMS START-->
-                                                            <div class="swiper-slide equip-config-section-item-carousel-slide">
-                                                                <div>
-                                                                    <?php if ($security['sistema_preduprezhdeniya_bokovogo_stolknoveniya_pri_vyezde_s_parkovki_zadnim_hodom_rccw']) : ?>
-                                                                        <svg>
-                                                                            <use xlink:href="<?php echo get_template_directory_uri() ?>/dist/images/dist/sprite.svg#check"></use>
-                                                                        </svg>
-                                                                    <?php else : ?>
-                                                                        <span class="d-block"> — </span>
-                                                                    <?php endif; ?>
-                                                                </div>
-                                                            </div>
-                                                        <?php endforeach; ?>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
                                         <div class="equip-config-section-item">
                                             <div class="equip-config-section-item-header">
                                                 Коленная подушка безопасности водителя
@@ -5697,6 +5670,34 @@ $GLOBALS['model_min_price'] = $model_min_price;
                                     <!-- ONE SECTION CAROUSEL ITEMS WRAPPER-->
                                     <div class="equip-config-section-items">
                                         <!-- ONE SECTION CAROUSEL ITEM-->
+                                        <div class="equip-config-section-item">
+                                            <div class="equip-config-section-item-header">
+                                                Система предупреждения бокового столкновения при выезде с парковки задним ходом (RCCW)
+                                            </div>
+                                            <div class="equip-config-section-item-carousel">
+                                                <!-- SWIPER STARTS-->
+                                                <div class="swiper-container equip-config-section-item-carousel-container">
+                                                    <div class="swiper-wrapper equip-config-section-item-carousel-wrapper">
+
+                                                        <?php foreach ($configs->posts as $post) : ?>
+                                                            <?php $drive_options = get_field('drive_wise_options', $post->ID); ?>
+                                                            <!-- SWIPER ITEMS START-->
+                                                            <div class="swiper-slide equip-config-section-item-carousel-slide">
+                                                                <div>
+                                                                    <?php if ($drive_options['sistema_preduprezhdeniya_bokovogo_stolknoveniya_pri_vyezde_s_parkovki_zadnim_hodom_rccw']) : ?>
+                                                                        <svg>
+                                                                            <use xlink:href="<?php echo get_template_directory_uri() ?>/dist/images/dist/sprite.svg#check"></use>
+                                                                        </svg>
+                                                                    <?php else : ?>
+                                                                        <span class="d-block"> — </span>
+                                                                    <?php endif; ?>
+                                                                </div>
+                                                            </div>
+                                                        <?php endforeach; ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                         <div class="equip-config-section-item">
                                             <div class="equip-config-section-item-header">
                                                 Система предотвращения фронтального столконовения (FCA) (уровень распознавания: автомобиль/пешеход)
