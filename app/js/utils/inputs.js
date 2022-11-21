04216b2db49ae316d40ebe2b4d98b7be371d0ce4
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2022-11-21 10:56:20 +0500

    asdasd

--- app/js/utils/inputs.js
+++ app/js/utils/inputs.js
@@ -7,77 +7,80 @@ import './dalacode-input-calc';
 import './dalacode-form-validator';
 
 $(() => {
-    // $('input#form-date').dalacodeInput({
-    //     'required': true,
-    // }),
-    $('input#form-name').dalacodeInput({
-        'required': true,
-    });
+  // $('input#form-date').dalacodeInput({
+  //     'required': true,
+  // }),
+  $('input#form-name').dalacodeInput({
+    required: true,
+  });
 
-    $('#form-vin').dalacodeInput({
-        'required': false,
-    });
+  $('#form-vin').dalacodeInput({
+    required: false,
+  });
 
-    $('#form-model').dalacodeInput({
-        'required': true,
-    });
+  $('#form-model').dalacodeInput({
+    required: true,
+  });
 
-    $('#form-year').dalacodeInput({
-        'required': true,
-    });
-    $('#form-email').dalacodeInput({
-        'required': false,
-    });
-    $('.select-form').dalacodeInput({
-        'required': true,
-    });
-    
-    if ($('input.input-phone').length) {
-        // eslint-disable-next-line no-unused-vars
-        const cleave = new Cleave('input.input-phone', {
-            'prefix': '+',
-            'blocks': [1, 1, 3, 3, 2, 2],
-            'delimiters': ['', ' (', ') ', '-', '-'],
-            'uppercase': true,
-            'numericOnly': true,
-        });
-    }
-    $('input.input-phone').dalacodeInput({
-        'required': true,
-        'limits': {
-            'minimum': 18,
-        },
-    });
-    if ($('input.input-phone-test-drive').length) {
-        // eslint-disable-next-line no-unused-vars
-        const testDriveInputPhone = new Cleave('input.input-phone-test-drive', {
-            'prefix': '+',
-            'blocks': [1, 1, 3, 3, 2, 2],
-            'delimiters': ['', ' (', ') ', '-', '-'],
-            'uppercase': true,
-            'numericOnly': true,
-        });
-    }
-    $('textarea.input-message').dalacodeTextarea({
-        'required': false,
-    });
+  $('#form-year').dalacodeInput({
+    required: true,
+  });
+  $('#form-email').dalacodeInput({
+    required: false,
+  });
+  $('#form-date').dalacodeInput({
+    required: true,
+  });
+  $('.select-form').dalacodeInput({
+    required: true,
+  });
 
-    $('input.input-checkbox').dalacodeCheckbox({
-        'required': true,
-        'content': {
-            'active': true,
-            'text': 'Проставляя отметку, в соответствии с Законом Республики Казахстан от 21 мая 2013 года № 94-V “О персональных данных и их защите” и иными нормативными правовыми актами Республики Казахстан, я даю свое безусловное согласие ТОО «Allur Motor Qazaqstan» [1], дилерам Kia[2], а также партнерам ТОО «Allur Motor Qazaqstan» в РК и за рубежом[3], на сбор, обработку, использование, обезличивание, распространение и трансграничную передачу моих персональных данных, таких как фамилия; имя; отчество; год; месяц, дата и место рождения; адрес, номер паспорта и сведения о дате выдачи паспорта и выдавшем его органе; образование, профессия, место работы и должность; домашний, рабочий и мобильный телефоны; адрес электронной почты. При этом сбор, обработка, использование, обезличивание, распространение и трансграничная передача моих персональных данных должна производится не противоречащими законодательству РК способами, в целях, связанных с возможностью предоставления информации о товарах и услугах, которые потенциально могут представлять интерес, информационных и рекламных материалов путем осуществления прямых контактов с помощью различных средств связи, включая, но, не ограничиваясь: почтовую рассылку, sms – рассылку, электронную почту, телефон, интернет, а также в целях сбора и обработки статистической информации и проведения маркетинговых исследований, в том числе общедоступных источниках.',
-        },
-    });    
-    $('input.input-checkbox-2').dalacodeCheckbox({
-        'required': false,
-        'content': {
-            'active': true,
-            'text': 'Я хочу, чтобы мне перезвонили и уточнили дату и время',
-        },
+  if ($('input.input-phone').length) {
+    // eslint-disable-next-line no-unused-vars
+    const cleave = new Cleave('input.input-phone', {
+      prefix: '+',
+      blocks: [1, 1, 3, 3, 2, 2],
+      delimiters: ['', ' (', ') ', '-', '-'],
+      uppercase: true,
+      numericOnly: true,
     });
+  }
+  $('input.input-phone').dalacodeInput({
+    required: true,
+    limits: {
+      minimum: 18,
+    },
+  });
+  if ($('input.input-phone-test-drive').length) {
+    // eslint-disable-next-line no-unused-vars
+    const testDriveInputPhone = new Cleave('input.input-phone-test-drive', {
+      prefix: '+',
+      blocks: [1, 1, 3, 3, 2, 2],
+      delimiters: ['', ' (', ') ', '-', '-'],
+      uppercase: true,
+      numericOnly: true,
+    });
+  }
+  $('textarea.input-message').dalacodeTextarea({
+    required: false,
+  });
 
-    $('.input-calc').dalacodeInputCalc();
-    
-    $('form').dalacodeForm();
+  $('input.input-checkbox').dalacodeCheckbox({
+    required: true,
+    content: {
+      active: true,
+      text: 'Проставляя отметку, в соответствии с Законом Республики Казахстан от 21 мая 2013 года № 94-V “О персональных данных и их защите” и иными нормативными правовыми актами Республики Казахстан, я даю свое безусловное согласие ТОО «Allur Motor Qazaqstan» [1], дилерам Kia[2], а также партнерам ТОО «Allur Motor Qazaqstan» в РК и за рубежом[3], на сбор, обработку, использование, обезличивание, распространение и трансграничную передачу моих персональных данных, таких как фамилия; имя; отчество; год; месяц, дата и место рождения; адрес, номер паспорта и сведения о дате выдачи паспорта и выдавшем его органе; образование, профессия, место работы и должность; домашний, рабочий и мобильный телефоны; адрес электронной почты. При этом сбор, обработка, использование, обезличивание, распространение и трансграничная передача моих персональных данных должна производится не противоречащими законодательству РК способами, в целях, связанных с возможностью предоставления информации о товарах и услугах, которые потенциально могут представлять интерес, информационных и рекламных материалов путем осуществления прямых контактов с помощью различных средств связи, включая, но, не ограничиваясь: почтовую рассылку, sms – рассылку, электронную почту, телефон, интернет, а также в целях сбора и обработки статистической информации и проведения маркетинговых исследований, в том числе общедоступных источниках.',
+    },
+  });
+  $('input.input-checkbox-2').dalacodeCheckbox({
+    required: false,
+    content: {
+      active: true,
+      text: 'Я хочу, чтобы мне перезвонили и уточнили дату и время',
+    },
+  });
+
+  $('.input-calc').dalacodeInputCalc();
+
+  $('form').dalacodeForm();
 });
