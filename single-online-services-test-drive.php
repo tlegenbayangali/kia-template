ba8688df77cb4e48c6cbda8c48f955cdb46465e3
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2022-12-14 14:51:34 +0500

    h1 in test drive

--- single-online-services-test-drive.php
+++ single-online-services-test-drive.php
@@ -1,24 +1,34 @@
 <?php
 get_header();
 ?>
-<?php get_template_part('template-parts/breadcrumbs'); ?>
+<?php
+get_template_part('template-parts/breadcrumbs'); ?>
 
-<?php if (get_field('forma_test-drajva', 'options')) : ?>
-    <?php if (!isset($_POST['selected_model'])) : ?>
-        <?php get_template_part('template-parts/test-drive', 'selector'); ?>
-    <?php else : ?>
-        <?php get_template_part('template-parts/test-drive', 'form'); ?>
-    <?php endif; ?>
-<?php else : ?>
+<?php
+if (get_field('forma_test-drajva', 'options')) : ?>
+    <?php
+    if (!isset($_POST[ 'selected_model' ])) : ?>
+        <?php
+        get_template_part('template-parts/test-drive', 'selector'); ?>
+    <?php
+    else : ?>
+        <?php
+        get_template_part('template-parts/test-drive', 'form'); ?>
+    <?php
+    endif; ?>
+<?php
+else : ?>
     <div class="section section-divided pos-r test-drive-section">
         <div class="container">
             <div class="pt-40 pb-40">
-                <h5 class="section-heading mb-4">Тест-драйв</h5>
+                <h1 class="section-heading mb-4">Тест-драйв</h1>
                 <h4>Где и когда Вам будет удобно пройти Тест–Драйв?</h4>
             </div>
-            <?php if (get_field('form-maps', 'options')) : ?>
+            <?php
+            if (get_field('form-maps', 'options')) : ?>
                 <?= get_field('form-maps', 'options') ?>
-            <?php endif; ?>
+            <?php
+            endif; ?>
             <div class="col-xl-8 col-lg-12">
                 <div class="content callback-col">
                     <div class="callback-form" style="margin: 0;">
@@ -30,7 +40,9 @@ get_header();
             </div>
         </div>
     </div>
-<?php endif; ?>
+<?php
+endif; ?>
 
 
-<?php get_footer(); ?>
+<?php
+get_footer(); ?>
