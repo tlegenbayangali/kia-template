4ce25da78bfd6d32f50f73eb223dd4db3fc806be
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2023-02-14 20:01:20 +0500

    360 is done

--- app/js/utils/car-single-colors.js
+++ app/js/utils/car-single-colors.js
@@ -3,24 +3,30 @@ import $ from 'jquery';
 window.jQuery = $;
 const mainParent = $('.model-sections-colors');
 const colorPickerExterior = $('.model-sections-colors-exterior .color-list').children();
+
 mainParent.find('.model-sections-colors-image img').attr('src', `${$(colorPickerExterior).data('src')}`);
 mainParent.find('.model-sections-colors-exterior-desc span').text(`${$(colorPickerExterior).data('text')}`);
 
 $('.model-sections-colors-exterior .color-list span:nth-child(1)').addClass('active');
 
 function colorPicker() {
-    colorPickerExterior.on('click', function colorPickerExteriorHandler() {
-        $(this).parents('.model-sections-colors')
-            .find('.model-sections-colors-image img')
-            .attr('src', `${$(this).data('src')}`);
-        $(this).parents('.model-sections-colors')
-            .find('.model-sections-colors-exterior-desc span')
-            .text(`${$(this).data('text')}`);
-        $(this).addClass('active');
-        $(this).siblings().removeClass('active');
-    });
+	colorPickerExterior.on('click', function colorPickerExteriorHandler() {
+		$(this).parents('.model-sections-colors')
+			.find('.cloudimage-360')
+			.eq(0)
+			.attr('data-folder', $(this).data('src'));
+
+		$(this).parents('.model-sections-colors')
+			.find('.model-sections-colors-exterior-desc span')
+			.text(`${$(this).data('text')}`);
+
+		$(this).addClass('active');
+		$(this).siblings().removeClass('active');
+
+		window.CI360.update('model-t60');
+	});
 }
 
 if ($('.model-sections-colors').length > 0) {
-    colorPicker();
+	colorPicker();
 }
--- single-models.php
+++ single-models.php
@@ -98,8 +98,18 @@ get_template_part('template-parts/content', 'header-models', ['parent_post' => $
                                 <!-- MAIN BLOCK FOR COLORS-->
                                 <div class="model-sections-colors-image-wrapper d-flex justify-content-center">
                                     <!-- COLORS IMAGE-->
-                                    <div class="model-sections-colors-image" id="model-sections-colors-image-0">
-                                        <img src="" alt="model"> <!-- COLOR IMAGE-->
+                                    <div class="model-sections-colors-image ">
+                                        <?php
+                                        $first_color = get_field('body_colors')[ 0 ][ 't60' ];
+                                        ?>
+                                        <div
+                                                class="cloudimage-360"
+                                                id="model-t60"
+                                                data-folder="<?= $first_color ?>"
+                                                data-filename-x="{index}.png"
+                                                data-amount-x="72"
+                                                data-spin-reverse="true"
+                                        ></div>
                                     </div> <!-- REQUIRED CLASS MODEL SECTION COLORS IMAGE-->
                                 </div>
                                 <div class="row mt-30">
@@ -109,8 +119,8 @@ get_template_part('template-parts/content', 'header-models', ['parent_post' => $
                                                 <div id="model-sections-colors-exterior-desc-0" class="model-sections-colors-exterior-desc">
                                                     Цвет:
                                                     <span>
-                                                    <!-- COLOR NAME-->
-                                                </span>
+                                                        <!-- COLOR NAME-->
+                                                    </span>
                                                 </div>
                                             </div>
                                             <!-- COLORS LIST EXTERIOR-->
@@ -118,18 +128,9 @@ get_template_part('template-parts/content', 'header-models', ['parent_post' => $
                                                 <?php
                                                 $current_model_colors = get_field('body_colors');
                                                 ?>
-                                                <?php
-                                                foreach ($current_model_colors as $color) : ?>
-                                                    <?php
-                                                    if ($color[ 'two_colors' ]) : ?>
-                                                        <span data-text="<?= $color[ 'body_color_name' ] ?>" data-src="<?= $color[ 'body_color_image' ] ?>" class="color-list-item" data-color="<?= $color[ 'body_colors_group' ][ 'body_colors_first' ] ?>" style="background-image: linear-gradient(180deg, <?= $color[ 'body_colors_group' ][ 'body_colors_first' ] ?> 50%, <?= $color[ 'body_colors_group' ][ 'body_colors_second' ] ?> 50%);"></span>
-                                                    <?php
-                                                    else : ?>
-                                                        <span data-text="<?= $color[ 'body_color_name' ] ?>" data-src="<?= $color[ 'body_color_image' ] ?>" class="color-list-item" data-color="<?= $color[ 'body_colors_group' ][ 'body_colors_first' ] ?>" style="background:<?= $color[ 'body_colors_group' ][ 'body_colors_first' ] ?>;"></span>
-                                                    <?php
-                                                    endif; ?>
-                                                <?php
-                                                endforeach; ?>
+                                                <?php foreach ($current_model_colors as $color) : ?>
+                                                    <span data-text="<?= $color[ 'body_color_name' ] ?>" data-src="<?= $color[ 't60' ] ?>" class="color-list-item" data-color="<?= $color[ 'body_colors_group' ][ 'body_colors_first' ] ?>" style="background:<?= $color[ 'body_colors_group' ][ 'body_colors_first' ] ?>;"></span>
+                                                <?php endforeach; ?>
                                             </div>
                                         </div>
                                     </div>
