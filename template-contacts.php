a988592603d16433a74b78c76afcf50a6ef49511
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2023-05-02 15:35:50 +0500

    contacts template fix

--- template-contacts.php
+++ template-contacts.php
@@ -73,7 +73,7 @@ get_template_part('template-parts/breadcrumbs'); ?>
                                     <?php
                                     foreach (get_field('dealer_info', 'options')[ 'dealer_phones' ] as $phone) : ?>
                                         <?php
-                                        if ($phone[ 'is_whatsapp' ]): ?>
+                                        if (isset($phone[ 'is_whatsapp' ])): ?>
                                             <a href="https://api.whatsapp.com/send?phone=<?= cleanPhone(
                                                 $phone[ 'dealer_phone' ]
                                             ) ?>&text=%F0%9F%91%8B%20%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%A5%D0%BE%D1%87%D1%83%20%D1%83%D0%B7%D0%BD%D0%B0%D1%82%D1%8C%20%D1%83%D0%B7%D0%BD%D0%B0%D1%82%D1%8C%20%D0%BF%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%B5%D0%B5%20%D0%BE%20%D0%BC%D0%BE%D0%B4%D0%B5%D0%BB%D0%B8%20..." class="right-number pos-r d-block">
