2f9c2678db64a73d6eb8bcee4b6e8a17d32c09b6
Tlegen Bayangali <tlegenbayangali@yandex.kz>
2023-07-03 18:44:06 +0500

    Commitment

--- template-parts/content-header-models.php
+++ template-parts/content-header-models.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once $_SERVER[ 'DOCUMENT_ROOT' ] . '/currentMonth.php';
+include_once $_SERVER[ 'DOCUMENT_ROOT' ] . '/wp-content/themes/kia/currentMonth.php';
 ?>
 <div class="header-model-wrapper">
     <div class="header-model" id="header-model">
